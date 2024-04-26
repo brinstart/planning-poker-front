cbfad12da8d1a55cb247bdda0e48c5be249eb639
nataly-enne <ennenataly@gmail.com>
2024-04-26 17:18:34 -0300

    chore: add some configs

---
+++ src/app.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import App from './app.tsx';
+
+describe('given App', () => {
+  describe('when render App', () => {
+    it('should render the heading title correctly', () => {
+      render(<App />);
+
+      expect(screen.getByText(/planning poker/i));
+    });
+  });
+});
