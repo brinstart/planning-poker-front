74eedb650bb97099584ed168c105a5e14dce139f
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2024-04-27 10:05:40 -0300

    chore: config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,49 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+//@ts-ignore
+import {resolve} from 'path'
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+
+//@ts-ignore
+const root = resolve(__dirname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
+    root: '.',
     environment: 'jsdom',
-    setupFiles: './test-setup.js',
+    include: ['{src,test}/**/*.test.tsx'],
+    exclude: [],
+    setupFiles: './vitest.setup.ts',
+    pool: 'vmThreads',
+    watch: false,
+    silent: true,
+    logHeapUsage: false,
+    passWithNoTests: true,
+    coverage: {
+      all: true,
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/main.tsx'],
+      provider: 'istanbul',
+      reporter: ['text-summary', 'html', 'lcov'],
+      thresholds: {
+        statements: 100
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(root, 'src'),
+      '~': resolve(root, 'test')
+    }
+  },
+  build: {
+    minify: true,
+    modulePreload: false
   }
 })
